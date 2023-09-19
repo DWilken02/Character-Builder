f880e5b47eee4db1a122604e45c3433ac4563281
DWilken02 <Dylan.Wilken06@gmail.com>
2023-09-19 15:40:48 -0700

    add select, radio, DOB, and color forms to html and JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+function handleSelect(event) {
+  event.preventDefault();
+  const selection = document.getElementById("gender").value;
+  document.querySelector("#output").innerText = "Gender: " + selection;
+
+};
+
+window.addEventListener("load", function() {
+  document.getElementById("select-form").addEventListener("submit", handleSelect);
+  document.getElementById("radio-form").addEventListener("submit", handleRadio);
+  document.getElementById("date-of-birth").addEventListener("submit", datechange);
+  document.getElementById("color-form").addEventListener("submit", favoritecolor);
+});
+
+function handleRadio(event) {
+  event.preventDefault();
+  const radioSelection = document.querySelector("input[name='trait']:checked").value;
+  document.querySelector("#output").innerText += "\nTrait: " + radioSelection;
+}
+
+function datechange(event) {
+  event.preventDefault();
+  const dob = document.querySelector("input[type='date']").value;
+  document.querySelector("#output").innerText += "\nBorn: " + dob;
+}
+
+function favoritecolor(event) {
+  event.preventDefault();
+  const color = document.querySelector("#color").value;
+  document.querySelector("#output").innerText += "\nColor: " + color;
+}
